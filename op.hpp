5882d9e610baff1cfd380f7eae5a770101dcb61f
Aika87 <akris023@ucr.edu>
2020-10-28 14:18:48 -0700

    Added op.hpp and rand.hpp and their tests

--- op.hpp
+++ op.hpp
@@ -2,12 +2,15 @@
 #define __OP_HPP__
 
 #include "base.hpp"
+#include <string> 
 
 class Op : public Base {
     public:
-        Op(double value) : Base() { }
-        virtual double evaluate() { return 0.0; }
-        virtual std::string stringify() { return ""; }
+        Op(double value) : val(value) {}
+        virtual double evaluate() { return val; }
+        virtual std::string stringify() { return std::to_string(val); }
+    private: 
+	double val; 
 };
 
 #endif //__OP_HPP__
--- op_test.hpp
+++ op_test.hpp
@@ -1,13 +1,39 @@
 #ifndef __OP_TEST_HPP__
 #define __OP_TEST_HPP__
 
-#include "gtest/gtest.h"
-
 #include "op.hpp"
+#include "gtest/gtest.h" 
+#include "rand.hpp"
+#include <stdlib.h>
+#include <string> 
 
 TEST(OpTest, OpEvaluateNonZero) {
     Op* test = new Op(8);
-    EXPECT_EQ(test->evaluate(), 8);
+    EXPECT_EQ("8.000000", test->stringify());
+    EXPECT_EQ(test->evaluate(), 8); 
 }
+
+TEST(OpTest, OpEvaluateNeg) {
+    Op* test = new Op(-8);
+    EXPECT_EQ("-8.000000", test->stringify());
+    EXPECT_EQ(test->evaluate(), -8);
+}
+
+TEST(OpTest, OpEvaluateZero) {
+    Op* test = new Op(0.0);
+    EXPECT_EQ("0.000000", test->stringify());
+    EXPECT_EQ(test->evaluate(), 0.0);
+}
+
+TEST(OpTest, EvalRand){
+    Rand* test = new Rand();
+    srand(0);  
+    int val = rand() % 100;
+    std::string str = std::to_string(val);
+    str+= ".000000";
+    EXPECT_EQ(str, test->stringify()); 
+    EXPECT_EQ(test->evaluate(), val); 
+}
+
 
 #endif //__OP_TEST_HPP__
